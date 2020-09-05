1f5df38ca631395fb6bf293ea26f5cb50ee6be6e
Pavel Jarkovský <jarkovsky.pavel@gmail.com>
2020-09-05 11:16:33 +0200

    První commit

--- SaveEdit/NacitamSave.Designer.cs
+++ SaveEdit/NacitamSave.Designer.cs
@@ -44,9 +44,9 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(15, 51);
+            this.progressBar1.Location = new System.Drawing.Point(12, 51);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(323, 23);
+            this.progressBar1.Size = new System.Drawing.Size(328, 23);
             this.progressBar1.Step = 5;
             this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
             this.progressBar1.TabIndex = 1;
@@ -55,7 +55,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 43);
+            this.ClientSize = new System.Drawing.Size(352, 82);
             this.ControlBox = false;
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.nacitamSaveLbl);
